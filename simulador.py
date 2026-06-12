209bb66a98a52ee530daca18232a3629c9f7fe28
Vinicius Soares <98933639+ViniVini0507@users.noreply.github.com>
2026-06-12 00:22:45 -0300

    remove divider desne

--- simulador.py
+++ simulador.py
@@ -149,7 +149,6 @@ if meta_amortizacao > 0:
 st.divider()
 
 # 5. Simulação de Orçamento: O 'Squeeze' da Obra
-st.divider()
 st.subheader("5. Simulação de Orçamento: O 'Squeeze' da Obra")
 st.markdown("Estabeleça o teto de gastos do mês. Conforme a evolução de obra 'esmaga' sua margem de poupança, o sistema aumenta automaticamente seu desembolso para garantir a reserva mínima estipulada.")
 
